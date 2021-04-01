4ce718b98325d7dada564faddfee7f80f27ed707
Gustavo <gustavovazco@gmail.com>
2021-04-01 16:18:53 -0600

    services description try

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,7 +126,11 @@ export default function index() {
 									<Card.Content extra>
 										<Card.Meta>
 											<Icon name="file text" />
-											Descripcion
+											Lorem Ipsum is simply dummy text of the printing and
+											typesetting industry. Lorem Ipsum has been the industry's
+											standard dummy text ever since the 1500s, when an unknown
+											printer took a galley of type and scrambled it to make a
+											type specimen book.
 										</Card.Meta>
 									</Card.Content>
 								</Card>
@@ -142,7 +146,11 @@ export default function index() {
 									<Card.Content extra>
 										<Card.Meta>
 											<Icon name="file text" />
-											Descripcion
+											Lorem Ipsum is simply dummy text of the printing and
+											typesetting industry. Lorem Ipsum has been the industry's
+											standard dummy text ever since the 1500s, when an unknown
+											printer took a galley of type and scrambled it to make a
+											type specimen book.
 										</Card.Meta>
 									</Card.Content>
 								</Card>
@@ -158,7 +166,11 @@ export default function index() {
 									<Card.Content extra>
 										<Card.Meta>
 											<Icon name="file text" />
-											Descripcion
+											Lorem Ipsum is simply dummy text of the printing and
+											typesetting industry. Lorem Ipsum has been the industry's
+											standard dummy text ever since the 1500s, when an unknown
+											printer took a galley of type and scrambled it to make a
+											type specimen book.
 										</Card.Meta>
 									</Card.Content>
 								</Card>
