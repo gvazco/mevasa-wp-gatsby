51ace2bfe1535a02719471221ec708f85aadd4ba
Gustavo <gustavovazco@gmail.com>
2021-04-20 17:37:01 -0500

    inor changes

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -7,33 +7,33 @@ import "moment/locale/es";
 import "./PostList.scss";
 
 export default function PostList(props) {
-  const { posts } = props;
-  console.log(posts);
+	const { posts } = props;
+	console.log(posts);
 
-  return (
-    <Grid className="post-list">
-      {map(posts, (post) => (
-        <Grid.Column key={post.id} mobile={16} tablet={16} computer={5}>
-          <Link to={`/blog/${post.slug}`}>
-            <Card className="post-list__item">
-              <Image src={post.featuredImage.node.localFile.publicURL} />
-              <Card.Content>
-                <Card.Header>{post.title}</Card.Header>
-              </Card.Content>
-              <Card.Content extra>
-                <Card.Meta>
-                  <Icon name="file text" />
-                  {post.Seo.seodescription}
-                </Card.Meta>
-                <Card.Meta>
-                  <Icon name="calendar alternate outline" />
-                  {moment(post.date).format("LL")}
-                </Card.Meta>
-              </Card.Content>
-            </Card>
-          </Link>
-        </Grid.Column>
-      ))}
-    </Grid>
-  );
+	return (
+		<Grid className="post-list">
+			{map(posts, (post) => (
+				<Grid.Column key={post.id} mobile={16} tablet={16} computer={5}>
+					<Link to={`/blog/${post.slug}`}>
+						<Card className="post-list__item">
+							<Image src={post.featuredImage.node.localFile.publicURL} />
+							<Card.Content>
+								<Card.Header>{post.title}</Card.Header>
+							</Card.Content>
+							<Card.Content extra>
+								<Card.Meta>
+									<Icon name="file text" />
+									{post.Seo.seodescription}
+								</Card.Meta>
+								<Card.Meta>
+									<Icon name="calendar alternate outline" />
+									{moment(post.date).format("LL")}
+								</Card.Meta>
+							</Card.Content>
+						</Card>
+					</Link>
+				</Grid.Column>
+			))}
+		</Grid>
+	);
 }
